ef2a05ca0ba92a550c62320622cf8ab7529c7309
cronjaeger <kitkat2.0@gmail.com>
2015-02-18 16:23:15 +0000

    added a c-function square.c to src. It efficiently squares an array of numbers...

---
+++ parallelComparisonpkg/src/square.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//int main(){
+//  return 0;
+//}
+
+void square(int *N , double *x){
+  /*Takes an array of size N and squares all entries.*/
+  int n = *N;
+  //size_t i = (size_t) n;
+  int i = (int) n;
+  for(i=0;i<n;++i){
+    x[i] = x[i] * x[i];
+  }
+}
